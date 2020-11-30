bc522501018d7f21dafc16ab1f5af6ba1689b6cd
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-11-29 23:54:04 -0600

    feat: add to serverless functions

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,20 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import signaling from './signaling';
 
+const serverless = require('serverless-http');
+
 import { log, LogLevel } from './log';
 
 export const createServer = (config): express.Application => {
   const app: express.Application = express();
-  // const signal = require('./signaling');
+
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  app.get('/protocol', (req, res) => res.json({useWebSocket: config.websocket}));
-  app.use('/signaling', signaling);
+  app.get('/.netlify/functions/api/protocol', (req, res) => res.json({useWebSocket: config.websocket}));
+  app.use('/.netlify/functions/api/signaling', signaling);
+
+  module.exports = app;
+  module.exports.handler = serverless(app);
 
   return app;
 };
